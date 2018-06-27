db911ee399b207b63ea4de075f43d6dc8c05b2f0
Simon Decaestecker <contact@simondecaestecker.com>
2018-06-27 13:23:38 +0200

    Adding iptables and some rules to the original Webterm image (router purpose)

---
+++ eurecom_netwi_router/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth2 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth2 -o eth0 -j ACCEPT
+iptables-save > /etc/iptables/rules.v4
+
+/bin/bash
